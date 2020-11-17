eeab98a0fd25b9ae5f13ad915209d28015dce386
Joao <joao.rsousa23@gmail.com>
2020-11-18 09:51:12 +1030

    feat: add style to main page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,13 @@
+import * as S from './styles'
+
 export const Main = () => (
-  <main>
-    <h1>react avancado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="img/logo.svg" alt="Atom image and advanced react" />
+    <S.Title>react avancado</S.Title>
+    <S.Description>Typescript, React JS and Styled components</S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="a developer in front of the code"
+    />
+  </S.Wrapper>
 )
